adac49647525b79ffc8353a217c08f1147c77922
djfm <fm.de.jouvencel@gmail.com>
2013-03-11 16:01:20 +0100

    Much better

--- change_keys.php
+++ change_keys.php
@@ -4,6 +4,72 @@ include_once 'Archive/Tar.php';
 
 @ini_set('display_errors', 'on');
 
+/************************************************/
+/*				HELPER FUNCTIONS 				*/
+/************************************************/
+
+//create a temporary directory
+//warning, subject to tiny race condition, couldn't find any better
+function tempdir()
+{
+    $tempfile=tempnam(sys_get_temp_dir(),'');
+    if(file_exists($tempfile))
+    { 
+    	unlink($tempfile);
+    	mkdir($tempfile);
+    }
+
+    if(is_dir($tempfile))return $tempfile;
+    else return false;
+}
+
+//recursively delete directory
+function rrmdir($dir) 
+{ 
+   if(is_dir($dir)) 
+   { 
+	     $objects = scandir($dir); 
+	     foreach ($objects as $object)
+	     { 
+	       	if ($object != "." && $object != "..")
+	       	{ 
+	         	if (filetype($dir."/".$object) == "dir")rrmdir($dir."/".$object); 
+	         	else unlink($dir."/".$object); 
+	       	} 
+    	} 
+    	reset($objects); 
+    	rmdir($dir); 
+   } 
+ }
+
+
+//file put contents with directory structure creation
+function file_put_contents_with_parents($path, $data)
+{
+	$dir = dirname($path);
+	if(!is_dir($dir))
+	{
+		mkdir($dir, 0777, true);
+	}
+	file_put_contents($path, $data);
+}
+
+//return an array with the (recursive) list of files in a directory with full path
+function file_list($dir)
+{
+	$files = array();
+	//lol, php
+	foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir)) as $path)
+	{
+		if(is_file($path) and !is_dir($path))$files[] = $path;
+	}
+	return $files;
+}
+
+/************************************************/
+/************************************************/
+/************************************************/
+
 if(count($argv) < 3)
 {
 	echo "Usage: " . basename($argv[0]) . " gzip_to_modify english_gzip_or_csv_to_use [english_gzip_or_csv_to_use ...]\n";
@@ -17,18 +83,6 @@ if(!file_exists($target))
 	echo "The file '$target' doesn't seem to exist!\n";
 	exit;
 }
-/*
-if(!file_exists($en))
-{
-	echo "The file '$en' doesn't seem to exist!\n";
-	exit;
-}
-if(basename($en) != "en.gzip")
-{
-	echo "The English pack to use must be named en.gzip!\n";
-	exit;
-}*/
-
 
 $en_changes_in_files = array();
 $en_uncontextualized_changes = array();
@@ -43,18 +97,21 @@ function readGZIPChanges($src, &$en_changes_in_files)
 {
 	$total = 0;
 	$en_gzip = new Archive_Tar($src);
+	$dir   = tempdir(); 
+	// for some reason Archive_Tar's extractInString is painfully slow on some platforms, so we extract in a temporary dir
+	$en_gzip->extract($dir);
 	foreach($en_gzip->listContent() as $desc)
 	{
 		$filename = $desc['filename'];
-		$source = $en_gzip->extractInString($filename);
+		$path     = "$dir/$filename";
+		$source   = file_get_contents($path);
 		$m = array();
 		if(preg_match('/\.php$/', $filename) and basename($filename) != 'index.php' and preg_match('/global\s+\$(_\w+)/', $source, $m))
 		{
 			$arr = $m[1];
-			echo "Parsing $filename to extract array $arr\n";
+			echo "Including $filename to extract array $arr\n";
 			
-			eval(substr($source, 5));
-			$count = 0;
+			include_once($path);
 			if(!isset($en_changes_in_files[$filename]))
 			{
 				$en_changes_in_files[$filename] = array();
@@ -70,21 +127,23 @@ function readGZIPChanges($src, &$en_changes_in_files)
 					{
 						//if($arr == '_LANGADM' and strpos($value, '&lt') !== false)echo "Changed: $value\n";
 						if(!isset($en_changes_in_files[$filename][$key]))$en_changes_in_files[$filename][$key] = array();
-						$en_changes_in_files[$filename][$key][$m[1] . $new_md5] = true;
-						$count += 1;
+						$en_changes_in_files[$filename][$key][$m[1] . $new_md5] = 0;
 					}
 				}
 			}
+			$count  = count($en_changes_in_files[$filename]);
 			$total += $count;
 			echo "$count of " . count($$arr) . " English strings were changed in the file $filename!\n\n";
 		}
 	}
+	rrmdir($dir);
 	return $total;
 }
 
 function readCSVChanges($src, &$en_uncontextualized_changes)
 {
-	$count = 0;
+	$count = count($en_uncontextualized_changes);
+
 	$f = fopen($src,'r');
 	$headers = fgetcsv($f);
 	$wanted  = array('Old Key', 'New English Text');
@@ -104,13 +163,12 @@ function readCSVChanges($src, &$en_uncontextualized_changes)
 			$new_md5 = md5(slashify($row['New English Text']));
 			if($md5 != $new_md5)
 			{
-				$en_uncontextualized_changes[$key] = $m[1] . $new_md5;
-				$count += 1;
+				$en_uncontextualized_changes[$key][$m[1] . $new_md5] = 0;
 			}
 		}
 	}
 
-	return $count;
+	return count($en_uncontextualized_changes) - $count;
 }
 
 function readChanges($src, &$en_changes_in_files, &$en_uncontextualized_changes)
@@ -154,49 +212,64 @@ if(file_exists($dst))
 
 $new_target_gzip = new Archive_Tar($dst, 'gz');
 
-$count = 0;
+$dir     = tempdir();
+$out_dir = tempdir();
+
+$target_gzip->extract($dir);
 foreach($target_gzip->listContent() as $desc)
 {
 	$filename = $desc['filename'];
+	$path     = "$dir/$filename";
 	echo "Changing keys in file $filename...\n";
 
 	$en_filename = str_replace("/$iso.php", '/en.php', str_replace("/$iso/", '/en/', $filename));
 	
-	$source = $target_gzip->extractInString($filename);
+	$source = file_get_contents($path);
 
 	$m = array();
 	if(preg_match('/global\s+\$(_\w+)/', $source, $m))
 	{
 		$nreplaced   =  0;
 		$arr = $m[1];
-		eval(substr($source, 5));
+		include_once($path);
 		$lang = $$arr;
 
 		if(isset($en_changes_in_files[$en_filename]))
 		{	
-			
-			
-			foreach($en_changes_in_files[$en_filename] as $old_key => $new_keys)
+			foreach($en_changes_in_files[$en_filename] as $old_key => &$new_keys)
 			{
-				foreach($new_keys as $new_key => $unused)
+				foreach($new_keys as $new_key => &$use_count)
 				{
 					if(isset($lang[$old_key]))
 					{
-						$lang[$new_key] = $lang[$old_key];
-						$nreplaced+=1;
+						$use_count += 1;
+
+						if(!isset($lang[$new_key]))
+						{
+							$lang[$new_key] = $lang[$old_key];
+							$nreplaced += 1;
+						}
 					}
 				}
 			}
 			
 		}
 
-		foreach($en_uncontextualized_changes as $old_key => $new_key)
+		foreach($en_uncontextualized_changes as $old_key => &$new_keys)
 		{
-			if(isset($lang[$old_key]))
-			{
-				$lang[$new_key] = $lang[$old_key];
-				$nreplaced+=1;
-			}	
+			foreach($new_keys as $new_key => &$use_count)
+				{
+					if(isset($lang[$old_key]))
+					{
+						$use_count += 1;
+
+						if(!isset($lang[$new_key]))
+						{
+							$lang[$new_key] = $lang[$old_key];
+							$nreplaced += 1;
+						}
+					}
+				}		
 		}
 
 		$source  = "<?php\n\nglobal \$$arr;\n\$$arr = array();";
@@ -208,11 +281,43 @@ foreach($target_gzip->listContent() as $desc)
 		$source .= "\n";
 
 		echo "Changed $nreplaced keys in file $filename!\n\n";
-		$count += $nreplaced;
 	}
 
-	$new_target_gzip->addString($filename, $source);
+	//$new_target_gzip->addString($filename, $source);
+	file_put_contents_with_parents("$out_dir/$filename", $source);
 
 }
+rrmdir($dir);
+$new_target_gzip->createModify(file_list($out_dir),'',$out_dir);
+rrmdir($out_dir);
 
-echo "\nChanged $count keys in $target (of $en_count)\n";
+$count         = 0;
+$show_not_used = function($arr) use (&$count)
+{
+	foreach($arr as $old_key => $new_keys)
+	{
+		$used = false;
+		foreach($new_keys as $new_key => $use_count)
+		{
+			if($use_count > 0)
+			{
+				$used   = true;
+				$count += 1;
+				break;
+			}
+		}
+		if(!$used)
+		{
+			echo "Key $old_key was never found, so not replaced!\n";
+		}
+	}
+};
+
+foreach($en_changes_in_files as $file => $changes)
+{
+	$show_not_used($changes);
+}
+
+$show_not_used($en_uncontextualized_changes);
+
+echo "\nChanged $count keys in '$target' (of $en_count)\n";
